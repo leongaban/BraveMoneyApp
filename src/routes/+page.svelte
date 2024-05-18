3016743c9be6a77e37e7ee2965e6a2bbb30b7275
Leon Gaban <leongaban@gmail.com>
2024-05-18 13:32:08 -0500

    binding variables on input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,13 +14,26 @@
     crypto: '',
   };
 
-  function onPointChange(event: any) {
+  let value = '';
+
+  function onPointChange(event: CustomEvent<number>) {
     console.log('Point changed', event.detail);
   }
+
+  // function onInput(event: Event) {
+  //   const input = event.target as HTMLInputElement;
+  //   value = input.value;
+  //   console.log('value', value);
+  // }
 </script>
 
 <h1>BraveMoney</h1>
 <h2>Your Investment Life Dashboard</h2>
+
+<!-- <input class="my-input" on:input={onInput} bind:value /> -->
+<input class="my-input" bind:value />
+
+<div>{value}</div>
 
 <Column status="first" {...obj} on:onPointChange={onPointChange} />
 
@@ -29,5 +42,11 @@
 <style>
   h1 {
     font-size: 4rem;
+  }
+
+  .my-input {
+    padding: 1rem;
+    border: 1px solid #333;
+    border-radius: 1rem;
   }
 </style>
