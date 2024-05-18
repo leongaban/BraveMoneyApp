42d535b105997facd3fc828739ec1916082191c6
Leon Gaban <leongaban@gmail.com>
2024-05-18 14:32:42 -0500

    configured paths to $lib and @

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import { Button } from '$lib/components/ui/button';
 </script>
 
 <h1>BraveMoney</h1>
 <h2>Your Investment Life Dashboard</h2>
+
+<Button>Click me</Button>
 
 <style>
   h1 {
